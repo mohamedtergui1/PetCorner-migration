3c707685a7f7252db12681d4cb1d571955ef5459
Macbook air m1 <macbookairm1@192.168.1.36>
2025-06-11 13:36:50 +0100

    fixed-make-it-work-onios

--- android/app/src/main/java/com/petcorner/MainApplication.kt
+++ android/app/src/main/java/com/petcorner/MainApplication.kt
@@ -13,7 +13,7 @@ import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
-
+  
   override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
@@ -27,6 +27,7 @@ class MainApplication : Application(), ReactApplication {
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
       }
 
